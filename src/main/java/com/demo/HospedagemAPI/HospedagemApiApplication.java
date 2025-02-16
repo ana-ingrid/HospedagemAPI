d104432b519431d647a6597fb3413a7b86a1adcc
Ana Clara <anaclaraa02pereira@gmail.com>
2025-02-16 17:35:41 -0300

    Projeto Inicializado

---
+++ src/main/java/com/demo/HospedagemAPI/HospedagemApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.HospedagemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedagemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedagemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/HospedagemAPI/HospedagemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.HospedagemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedagemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
