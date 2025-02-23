35db18b54c5b2c066a61aee578615b78cfa141bc
Ana Clara <anaclaraa02pereira@gmail.com>
2025-02-22 21:54:40 -0300

    Alteração da modelagem de classe e criação de feature

--- src/main/java/com/demo/hospedagem/beneficio/model/Beneficio.java
+++ src/main/java/com/demo/hospedagem/beneficio/model/Beneficio.java
@@ -1,41 +1,15 @@
 package com.demo.hospedagem.beneficio.model;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 
 public class Beneficio {
 
     private Integer id_beneficio;
     private String nome;
     private String descricao;
-
-    public Beneficio(Integer id_beneficio, String nome, String descricao) {
-        this.id_beneficio = id_beneficio;
-        this.nome = nome;
-        this.descricao = descricao;
-    }
-
-
-    public Integer getId_beneficio() {
-        return id_beneficio;
-    }
-
-    public void setId_beneficio(Integer id_beneficio) {
-        this.id_beneficio = id_beneficio;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
--- src/main/java/com/demo/hospedagem/quarto/model/Quarto.java
+++ src/main/java/com/demo/hospedagem/quarto/model/Quarto.java
@@ -1,12 +1,13 @@
 package com.demo.hospedagem.quarto.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
-
+@Data
+@AllArgsConstructor
 public class Quarto {
     private int id_quarto;
     private int id_categoria;
@@ -16,70 +17,4 @@ public class Quarto {
     private String status;
     private BigDecimal valor_diaria;
 
-    public Quarto(int capacidade, int id_categoria, int id_quarto, int numero, String status, String tipo, BigDecimal valor_diaria) {
-        this.capacidade = capacidade;
-        this.id_categoria = id_categoria;
-        this.id_quarto = id_quarto;
-        this.numero = numero;
-        this.status = status;
-        this.tipo = tipo;
-        this.valor_diaria = valor_diaria;
-    }
-
-
-    public int getCapacidade() {
-        return capacidade;
-    }
-
-    public void setCapacidade(int capacidade) {
-        this.capacidade = capacidade;
-    }
-
-    public int getId_categoria() {
-        return id_categoria;
-    }
-
-    public void setId_categoria(int id_categoria) {
-        this.id_categoria = id_categoria;
-    }
-
-    public int getId_quarto() {
-        return id_quarto;
-    }
-
-    public void setId_quarto(int id_quarto) {
-        this.id_quarto = id_quarto;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public BigDecimal getValor_diaria() {
-        return valor_diaria;
-    }
-
-    public void setValor_diaria(BigDecimal valor_diaria) {
-        this.valor_diaria = valor_diaria;
-    }
 }
